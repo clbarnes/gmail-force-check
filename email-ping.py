b711dd4e40a14c97d59a8a02a3c37fae986f81cb
clbarnes <barnesc@janelia.hhmi.org>
2016-09-14 14:49:31 -0400

    changes

--- email-ping.py
+++ email-ping.py
@@ -2,18 +2,33 @@
 import smtplib
 import time
 from email.mime.text import MIMEText
+import json
+import base64 as b64
 
-to_list = ('',)  # add recipient (your remote account) here
-from_email = ''  # email from which the e-mail is sent; must be accepted by SMTP
 
-s = smtplib.SMTP_SSL('')  # SMTP address
-s.login('', '')  # ('smtp login', 'smtp password')
+def get_settings():
+    with open('settings.json') as f:
+        return json.load(f)
+
+
+def make_from_address(from_username, from_suffix):
+    user, server = from_username.split('@')
+    return '{}+{}@{}'.format(user, from_suffix, server)
+
+
+settings = get_settings()
+
+to_list = settings['to_list']  # add recipient (your remote account) here
+from_email = make_from_address(settings['from_username'], settings['from_suffix'])  # email from which the e-mail is sent; must be accepted by SMTP
+
+s = smtplib.SMTP_SSL(settings['smtp_address'])  # SMTP address
+s.login(settings['from_username'], b64.b64decode(settings['enc_password']).decode())  # ('smtp login', 'smtp password')
 
 for to in to_list:
     msg = MIMEText('server status: OK')
-    msg['Subject'] = 'Server status '+time.ctime()
+    msg['Subject'] = '{} {}'.format(settings['subject'], time.ctime())
     msg['From'] = from_email
     msg['To'] = to
-    print msg.as_string()
+    print(msg.as_string())
     s.sendmail(from_email, [to], msg.as_string())
 
